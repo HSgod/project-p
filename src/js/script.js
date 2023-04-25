bb392c8cefa107d4244e3921d528e3bf98298547
HSgod <hsgodkontakt@gmail.com>
2023-04-25 13:16:37 +0200

    Create dropdown menu
    Signed-off-by: HSgod <hsgodkontakt@gmail.com>

--- src/js/script.js
+++ src/js/script.js
@@ -60,6 +60,7 @@ const select = {
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
+      thisProduct.initAccordion();
 
       console.log('new product:', thisProduct);
     }
@@ -78,6 +79,31 @@ const select = {
 
       /* add element to menu */
       menuContainer.appendChild(thisProduct.element);
+    }
+
+    initAccordion() {
+      const thisProduct = this;
+      
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = document.querySelector(select.menuProduct.clickable);
+
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+
+        /* prevent default action for event */
+        event.preventDefault();
+
+        /* find active product (product that has active class) */
+        const activeProduct = document.querySelector(classNames.menuProduct.wrapperActive);
+
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if (activeProduct && activeProduct != thisProduct.element) {
+          thisProduct.element.classList.remove('active');
+        }
+
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle('active');
+      });
     }
   }
 
