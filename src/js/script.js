02cb9d9d43a696fa5744cfd3ef37756bc90ce31a
HSgod <hsgodkontakt@gmail.com>
2023-05-01 13:43:44 +0200

    Create dropdown for cart section
    Signed-off-by: HSgod <hsgodkontakt@gmail.com>

--- src/js/script.js
+++ src/js/script.js
@@ -284,6 +284,32 @@ const select = {
     }
   }
 
+  class Cart {
+    constructor(element) {
+      const thisCart = this;
+
+      thisCart.products = [];
+      thisCart.getElements(element);
+      thisCart.initActions();
+    }
+
+    getElements(element) {
+      const thisCart = this;
+
+      thisCart.dom = {};
+      thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions() {
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener('click', function() {
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+    }
+  }
+
   const app = {
     initMenu: function() {
       const thisApp = this;
@@ -298,6 +324,13 @@ const select = {
       thisApp.data = dataSource;
     },
 
+    initCart: function() {
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
+
     init: function() {
       const thisApp = this;
       //console.log('*** App starting ***');
@@ -308,6 +341,7 @@ const select = {
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
   };
 
